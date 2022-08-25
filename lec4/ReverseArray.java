fda9a9b1520fe6ac5c5efd2fdff6989d427c7626
AnujRuhela7 <ruhela7777@gmail.com>
2022-08-25 23:48:33 +0530

    Added function to reverse array elements in a range

--- lec4/ReverseArray.java
+++ lec4/ReverseArray.java
@@ -4,6 +4,7 @@ public class ReverseArray
 {
     public static void reverseArray(int []arr) /* Reversal of Array by two pointer method */
     {
+        // Function to Reverse Full Array
         int j = arr.length-1;
         int i = 0;
         while (i < j)
@@ -18,6 +19,25 @@ public class ReverseArray
             System.out.print(value + "\t");
         }
     }
+
+    public static void reverseArray(int []arr, int begin, int end) /* Reversal of Array by two pointer method */
+    {
+        // Function to Reverse Array Elements in a range
+        int j = end;
+        int i = begin;
+        while (i < j)
+        {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+        for (int value : arr) {
+            System.out.print(value + "\t");
+        }
+    }
+
     public static void main(String[] args)
     {
         int []arr = {4,-23,7,6,-7,10,2,9};
